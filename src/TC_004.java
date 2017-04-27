6ccb1efeab9ccc677d4fd5740af88d13d1801957
Dhanush27 <dhanushashok27@gmail.com>
2017-04-27 21:35:04 +0530

    Add files via upload

---
+++ src/TC_004.java
@@ -0,0 +1,52 @@
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.gargoylesoftware.htmlunit.javascript.host.Window;
+
+public class TC_004 {
+
+	public static void main(String[] args) throws InterruptedException {
+		String driverpath = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+
+		System.out.println("launching browser");
+		System.setProperty("webdriver.gecko.driver",
+				"C:\\Users\\dhanusha\\Dhanush Project learnings\\Dhanush\\Jars\\geckodriver-v0.16.1-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.metrobankonline.co.uk/");
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//a[@id='js-mbCookieNotice-button']")).click();
+		driver.findElement(By.xpath("//a[@href='/about-us/']")).click();
+		Thread.sleep(3001);
+		driver.findElement(By.xpath("//a[@href='/about-us/events/']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@id='search-field']")).sendKeys("Grand");
+		driver.findElement(By.xpath("//button[@class='searchResultsPage-form-submit']")).click();
+		if (driver.findElement(By.xpath("//*[contains(text(), 'Grand')]")) != null) {
+			System.out.println("validation passed");
+		}
+		((JavascriptExecutor) driver).executeScript("window.scrollBy(0,300)");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[contains(text(),'Read')]")).click();
+		
+		String read=driver.findElement(By.xpath("//div[@class='contentBlock contentBlock_event ']")).getText();
+		System.out.println(read);
+		
+		
+		
+		
+		
+		
+		
+		//driver.findElement(By.xpath("//a[@href='/about-us/events/']")).click();
+		
+		
+	}
+}
+		
